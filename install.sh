d66ab827a624d8c7733e8cd7b803af55cb4aa1f7
Francesco Di Sales <f.disales@hotmail.com>
2021-06-20 02:41:51 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,21 @@
-echo "must run as root"
+user=''
 
-user=""
-
-echo -n "insert the name of the user who must have sudo privileges: "
-read $user
-
-if [ '$user' != "" ]
+if [ $EUID -eq 0 ]
 then
-  sed -i "s/user/$user/g" sudoers
-  cp sudoers /etc/sudoers
-else
-  echo "please insert the name of the user"
-fi
 
+  echo -n "insert the name of the user who must have sudo privileges: "
+  read $user
+
+  if [ '$user' != '' ]
+  then
+    sed -i "s/user/$user/g" sudoers
+    cp sudoers /etc/sudoers
+    echo "informations written on /etc/sudoers"
+  else
+    echo "please insert the name of the user"
+  fi
+ 
+else
+  echo "must run as root"
+fi
 

d40209349ac4d94fa435ff07d19fa42f999db16e
FrancescoDiSalesGithub <f.disales@hotmail.com>
2021-06-20 15:30:12 +0000

    fix wrong read variable

--- install.sh
+++ install.sh
@@ -4,10 +4,11 @@ if [ $EUID -eq 0 ]
 then
 
   echo -n "insert the name of the user who must have sudo privileges: "
-  read $user
+  read user
 
   if [ '$user' != '' ]
   then
+    echo $user	  
     sed -i "s/user/$user/g" sudoers
     cp sudoers /etc/sudoers
     echo "informations written on /etc/sudoers"
